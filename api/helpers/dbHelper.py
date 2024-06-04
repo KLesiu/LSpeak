7c182ec06d0c319b6c8d414da1af4e9e05297a22
Jakub Leśkiewicz <kubaxl222@gmail.com>
2024-06-04 23:39:52 +0200

    Add compare_text and load_data_by_level functions, one of them compare text which has been sent by user and text from database then return mistakes, the second function return data from db.json including level which has been sent by frontend

---
+++ api/helpers/dbHelper.py
@@ -0,0 +1,6 @@
+import json
+from typing import Dict, List
+# Wczytaj dane z pliku JSON
+def load_data() -> Dict[str, List[Dict[str, str]]]:
+    with open('./../db.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
--- api/services/services.py
+++ api/services/services.py
@@ -1,12 +1,35 @@
 import assemblyai as aai
 import os
+from io import BytesIO
+from fuzzywuzzy import process
+from fastapi import  HTTPException
+from helpers.dbHelper import load_data
 from dotenv import load_dotenv
 load_dotenv()
 aai.settings.api_key = os.getenv('ASSEMBLYAI_KEY')
 
 
-def transcribe_to_text(file:str):
+def transcribe_to_text(file:bytes)->str:
     transcriber = aai.Transcriber()
-    transcript = transcriber.transcribe(file)
+    audio_data = BytesIO(file)
+    transcript = transcriber.transcribe(audio_data)
     return transcript.text
+
+def compare_text(textBase:str,text:str)-> list:
+    textBase_words = textBase.split()
+    text_words = text.split()
+    mistakes = []
+    for word in text_words:
+        best_match = process.extractOne(word,textBase_words)
+        if(best_match[1] < 95):
+            mistakes.append(word)
+    return mistakes
+
+def load_data_by_level(level:str)->list:
+    data = load_data()
+    if(level in data):
+        return data[level]
+    return HTTPException(status_code=404,detail='Level not found')
+ 
+
 
