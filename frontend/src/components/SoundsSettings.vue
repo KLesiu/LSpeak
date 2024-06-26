b38aa573f99958a9407627430022212c94d8aa77
Lesiuuu <j.leskiewicz@tdz.com.pl>
2024-06-26 09:48:51 +0200

    input range update styles

--- frontend/src/components/SoundsSettings.vue
+++ frontend/src/components/SoundsSettings.vue
@@ -11,18 +11,20 @@
             </div>
             <div class="sound-settings__options-container__option">
                 <span>Volume</span>
-                <input v-model="soundValue" @change="changeSoundValue" type="range">
+                <input v-model="soundValue" @change="changeSoundValue" type="range" min="0" max="100" step="1" ref="inputRange">
             </div>
         </section>
     </section>
 </template>
 
 <script lang="ts" setup>
-import { Ref, onMounted, ref } from 'vue';
+import { Ref, nextTick, onMounted, ref } from 'vue';
 
 
 const soundValue:Ref<number> = ref(50);
 const isSoundEffectOn:Ref<boolean> = ref(true);
+const inputRange = ref<HTMLInputElement | null>(null);
+
 
 const changeSoundValue = (event: Event):void => {
     const target = event.target as HTMLInputElement;
@@ -33,10 +35,32 @@ const changeSoundEffect = (value:boolean):void => {
     localStorage.setItem('isSoundEffectOn', value.toString());
 }
 
-onMounted(() => {
+
+
+const setBackgroundSize = (input: HTMLInputElement): void => {
+    if(!input) return;
+    input.style.setProperty("--background-size", `${getBackgroundSize(input)}%`);
+    
+}
+
+const getBackgroundSize = (input: HTMLInputElement): number => {
+    const min = +input.min || 0;
+    const max = +input.max || 100;
+    const value = +input.value;
+    const size = ((value - min) / (max - min)) * 100;
+    return size;
+}
+
+onMounted(async() => {
     const soundValueFromLocalStorage = localStorage.getItem('soundValue');
     const isSoundEffectOnFromLocalStorage = localStorage.getItem('isSoundEffectOn');
     if(soundValueFromLocalStorage) soundValue.value = parseInt(soundValueFromLocalStorage);
     if(isSoundEffectOnFromLocalStorage) isSoundEffectOn.value = isSoundEffectOnFromLocalStorage === 'true';
+    await nextTick();
+    
+    if (inputRange.value) {
+        setBackgroundSize(inputRange.value);
+        inputRange.value.addEventListener("input", () => setBackgroundSize(inputRange.value!));
+    }
 })
 </script>
